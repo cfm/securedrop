bf46dca231b090e1135474902bf2db0b460bc36e
Cory Francis Myers <cory@freedom.press>
2025-10-10 13:10:41 -0700

    feat(EventResult): "status" field provides tuple of code and optional message
    thread: https://github.com/freedomofpress/securedrop/pull/7672#discussion_r2402376395

--- securedrop/journalist_app/api2/events.py
+++ securedrop/journalist_app/api2/events.py
@@ -25,10 +25,10 @@ class EventHandler:
             else:
                 raise TypeError("invalid event target")
 
-        except (TypeError, ValueError):
+        except (TypeError, ValueError) as e:
             return EventResult(
                 event_id=event.id,
-                status=EventStatusCode.BadRequest,
+                status=(EventStatusCode.BadRequest, str(e)),
             )
 
         try:
@@ -36,7 +36,7 @@ class EventHandler:
         except AttributeError:
             return EventResult(
                 event_id=event.id,
-                status=EventStatusCode.NotImplemented,
+                status=(EventStatusCode.NotImplemented, f"no handler for event type: {event.type}"),
             )
 
         return handler(event)
@@ -56,7 +56,7 @@ class EventHandler:
 
         return EventResult(
             event_id=event.id,
-            status=EventStatusCode.OK,
+            status=(EventStatusCode.OK, None),
             sources={source.uuid: source},
             items={reply.uuid: reply},
         )
